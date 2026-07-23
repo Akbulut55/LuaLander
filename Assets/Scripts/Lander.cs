c1260d617a737cb5d6caa17e26a0d97c94242538
Akbulut55 <karbarkurlas@gmail.com>
2026-07-23 09:22:49 +0300

    Initial commit: add Unity lunar lander project

---
+++ Assets/Scripts/Lander.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Lander : MonoBehaviour
+{
+    private Rigidbody2D landerRigidbody2D;
+
+    private void Awake()
+    {
+        landerRigidbody2D = GetComponent<Rigidbody2D>();
+
+        Debug.Log(Vector2.Dot(new Vector2(0, 1), new Vector2(0, 1)));
+        Debug.Log(Vector2.Dot(new Vector2(0, 1), new Vector2(.5f, .5f)));
+        Debug.Log(Vector2.Dot(new Vector2(0, 1), new Vector2(1, 0)));
+        Debug.Log(Vector2.Dot(new Vector2(0, 1), new Vector2(0, -1)));
+    }
+
+    private void FixedUpdate()
+    {
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            float force = 700f;
+            landerRigidbody2D.AddForce(force * transform.up * Time.deltaTime);
+        }
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            float turnSpeed = +100f;
+            landerRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
+        }
+        if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            float turnSpeed = -100f;
+            landerRigidbody2D.AddTorque(turnSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        if (!collision2D.gameObject.TryGetComponent(out LandingPad landingPad))
+        {
+            Debug.Log("Crashed on the terrain");
+            return;
+        }
+
+        float softLandingVelocityMagnitude = 4f;
+        float relativeVelocityMagnitude = collision2D.relativeVelocity.magnitude;
+        if (relativeVelocityMagnitude > softLandingVelocityMagnitude)
+        {
+            Debug.Log("Landed too hard!");
+            return;
+        }
+
+        float dotVector = Vector2.Dot(Vector2.up, transform.up);
+        float minDotVector = .90f;
+        if (dotVector < minDotVector)
+        {
+            Debug.Log("Landed on too steep angle.");
+            return;
+        }
+
+        Debug.Log("Successful landing.");
+
+        float maxScoreAmountLandingAngle = 100;
+        float maxScoreDotVectorMultiplier = 10f;
+        float landingAngleScore = maxScoreAmountLandingAngle - Mathf.Abs(dotVector - 1f) * maxScoreDotVectorMultiplier * maxScoreAmountLandingAngle;
+
+        float maxScoreAmountLandingSpeed = 100;
+        float landingSpeedScore = (softLandingVelocityMagnitude - relativeVelocityMagnitude) * maxScoreAmountLandingSpeed;
+
+        Debug.Log("LandingAngleScore:" + landingAngleScore);
+        Debug.Log("LandingSpeedScore:" + landingSpeedScore);
+    }
+}
